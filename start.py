21e5c4169b417e0c3c953c894e220b0acf4385cb
SergeyLebidko <it.sergeyler@mail.ru>
2020-07-26 19:32:20 +0300

    Добавлен класс Polymino и возможность перетаскивания его экземпляров

--- start.py
+++ start.py
@@ -22,10 +22,49 @@ GRID_COLOR = (120, 120, 120)
 
 class Monomino(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, row, col, color_number):
         super().__init__()
-        self.image = pygame.image.load(f'{SPRITES_FOLDER}/3.png')
+        self.image = pygame.image.load(f'{SPRITES_FOLDER}/{color_number}.png')
         self.rect = self.image.get_rect()
+        self.rect.x = CELL_SIZE + (CELL_SIZE * col) + 1
+        self.rect.y = CELL_SIZE + (CELL_SIZE * row) + 1
+
+
+class Polymino:
+
+    def __init__(self):
+        self.monomino_list = []
+
+    def add(self, monomino):
+        self.monomino_list.append(monomino)
+
+    def move_ip(self, dx, dy):
+        for monomino in self.monomino_list:
+            monomino.rect.move_ip((dx, dy))
+
+    def collidepoint(self, x, y):
+        return any([monomino.rect.collidepoint((x, y)) for monomino in self.monomino_list])
+
+    def blit(self, surface):
+        for monomino in self.monomino_list:
+            surface.blit(monomino.image, monomino.rect)
+
+
+def create_polymino(data, color_number):
+    polymino = Polymino()
+
+    data = data.split('|')
+    row = 0
+    for line in data:
+        col = 0
+        for element in line:
+            if element != '_':
+                monomino = Monomino(row, col, color_number)
+                polymino.add(monomino)
+            col += 1
+        row += 1
+
+    return polymino
 
 
 def draw_grid(surface):
@@ -37,7 +76,7 @@ def draw_grid(surface):
 
 
 def main():
-    create_sprites(CELL_SIZE - 2)
+    create_sprites(CELL_SIZE - 1)
     create_square_sprite(16, (255, 0, 0), 'icon')
 
     pygame.init()
@@ -48,7 +87,7 @@ def main():
 
     clock = pygame.time.Clock()
 
-    square = Monomino()
+    polymino = create_polymino('_X__|XX__|_XX_|__X_', 5)
     drag_flag = False
 
     while True:
@@ -59,17 +98,17 @@ def main():
                 exit()
 
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == pygame.BUTTON_LEFT:
-                if square.rect.collidepoint(event.pos):
+                if polymino.collidepoint(*event.pos):
                     drag_flag = True
 
             if event.type == pygame.MOUSEBUTTONUP and event.button == pygame.BUTTON_LEFT:
                 drag_flag = False
 
             if event.type == pygame.MOUSEMOTION and drag_flag:
-                square.rect.move_ip(event.rel)
+                polymino.move_ip(*event.rel)
 
         draw_grid(sc)
-        sc.blit(square.image, square.rect)
+        polymino.blit(sc)
 
         pygame.display.update()
         clock.tick(FPS)
